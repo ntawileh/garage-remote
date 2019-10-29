007280eec12f22abe97d9aacfc95adb147d8007a
Nadim Tawileh <nadim@tawileh.com>
2019-10-29 16:03:52 -0700

    boot on startup and periodic reboot

---
+++ etc/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+cd /home/pi/garage-remote
+git pull
+source .env.prod
+if [ -z "$SERVER_NAME" ]; then
+        echo "Missing environment variables"
+        exit 1
+fi 
+docker-compose build
+docker-compose up -d
+
+exit 0
